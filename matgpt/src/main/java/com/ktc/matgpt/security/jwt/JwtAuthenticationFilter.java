73261beabea759af66ee4d69f3022aeda9c0df31
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-10-01 12:35:42 +0900

    feat: JWT 인증 필터 구현
    Request Header에서 토큰을 추출하고, 토큰의 유효성을 검사하여 SecurityContext에 Authentication 정보를 저장하는 필터를 구현하였습니다.

---
+++ matgpt/src/main/java/com/ktc/matgpt/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,58 @@
+package com.ktc.matgpt.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private static final Logger log = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+
+    public static final String AUTHORIZATION_HEADER_NAME = "Authorization";
+    public static final String BEARER_TOKEN_PREFIX = "Bearer ";
+
+    private final TokenProvider tokenProvider;
+
+    // 실제 필터링 로직은 doFilterInternal 에 들어감
+    // JWT 토큰의 인증 정보를 현재 쓰레드의 SecurityContext 에 저장하는 역할 수행
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+
+        try {
+            // 1. Request Header 에서 토큰을 꺼냄
+            String jwt = getJwtFromRequest(request);
+
+            // 2. validateToken 으로 토큰 유효성 검사
+            // 정상 토큰이면 해당 토큰으로 Authentication 을 가져와서 SecurityContext 에 저장
+            if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+                Authentication authentication = tokenProvider.getAuthentication(jwt, request);
+                log.debug("Authentication 정보: {}", authentication);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception e){
+            log.error("Could not set user authentication in security context", e);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getJwtFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER_NAME);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_TOKEN_PREFIX)) {
+            return bearerToken.substring(7, bearerToken.length());
+        }
+        return null;
+    }
+}
