e45e77bed099724a83ab036c144147397e7f7aac
pigglehyun <sarah0661@pusan.ac.kr>
2023-10-06 14:33:00 +0900

    feat: FindAllStoreDTO 생성

--- matgpt/src/main/java/com/ktc/matgpt/store/StoreService.java
+++ matgpt/src/main/java/com/ktc/matgpt/store/StoreService.java
@@ -5,17 +5,24 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class StoreService {
+
     private final StoreJPARepository storeJPARepository;
     public Store findById(Long id) {
         return storeJPARepository.findById(id).orElseThrow();
     }
 
-    public List<Store> findAll() {
-        return storeJPARepository.findAll();
+    public List<StoreResponse.FindAllStoreDTO> findAll() {
+        List<Store> store = storeJPARepository.findAll();
+
+        List<StoreResponse.FindAllStoreDTO> responseDTOs = store.stream()
+                .map(s -> new StoreResponse.FindAllStoreDTO(s))
+                        .collect(Collectors.toList());
+        return responseDTOs;
     }
 
 }
