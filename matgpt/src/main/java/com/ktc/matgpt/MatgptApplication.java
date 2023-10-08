1c961b5dc98ad38af43ef8e329b14001c4c4e90c
jinc20 <wh5971@pusan.ac.kr>
2023-10-08 18:31:19 +0900

    Chore: Auditing, S3 관련 설정

--- matgpt/src/main/java/com/ktc/matgpt/MatgptApplication.java
+++ matgpt/src/main/java/com/ktc/matgpt/MatgptApplication.java
@@ -1,8 +1,17 @@
 package com.ktc.matgpt;
 
+import com.ktc.matgpt.feature_review.store.Store;
+import com.ktc.matgpt.feature_review.store.StoreJPARepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Arrays;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class MatgptApplication {
 
