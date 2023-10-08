57b593be6758979d6ed5c41ddc1d6b16c0888b62
jinc20 <wh5971@pusan.ac.kr>
2023-10-08 18:09:23 +0900

    Store 관련기능 임시 구현

---
+++ matgpt/src/main/java/com/ktc/matgpt/feature_review/store/StoreJPARepository.java
@@ -0,0 +1,9 @@
+package com.ktc.matgpt.feature_review.store;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StoreJPARepository extends JpaRepository<Store,Long> {
+
+}
