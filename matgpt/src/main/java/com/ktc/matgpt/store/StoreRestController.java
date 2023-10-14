a5e1df879d99a58543803d694a079f01750ed4e2
pigglehyun <sarah0661@pusan.ac.kr>
2023-10-14 16:17:15 +0900

    feat: cursor paging과 sorting을 추가한 Controller 작성

--- matgpt/src/main/java/com/ktc/matgpt/store/StoreRestController.java
+++ matgpt/src/main/java/com/ktc/matgpt/store/StoreRestController.java
@@ -4,10 +4,12 @@ package com.ktc.matgpt.store;
 
 import com.ktc.matgpt.utils.ApiUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,17 +19,29 @@ import java.util.stream.Collectors;
 @RestController
 public class StoreRestController {
     private final StoreService storeService;
+    private static final int PAGE_DEFAULT_SIZE = 5;
 
 
     @GetMapping("/stores")
-    public ResponseEntity<?> findAll() {
-        List<StoreResponse.FindAllStoreDTO> responseDTOs = storeService.findAll();
+    public ResponseEntity<?> findAllByPage(@RequestParam(value = "sort", defaultValue = "id") String sort,
+                                           @RequestParam(value = "cursor", defaultValue = "6") Long cursor) {
+        List<StoreResponse.FindAllStoreDTO> responseDTOs = storeService.findAllByPage(sort,cursor, PageRequest.of(0, PAGE_DEFAULT_SIZE));
         ApiUtils.ApiSuccess<?> apiResult = ApiUtils.success(responseDTOs);
         return ResponseEntity.ok(apiResult);
     }
 
+
+//    @GetMapping("/stores/dis")
+//    public ResponseEntity<?> findAllbyDis( @RequestParam("user_latitude") double latitude,
+//                                           @RequestParam("user_longitude") double longitude) {
+//        List<StoreResponse.FindAllStoreDTO> responseDTOs = storeService.findAllByDistance(latitude,longitude);
+//        ApiUtils.ApiSuccess<?> apiResult = ApiUtils.success(responseDTOs);
+//        return ResponseEntity.ok(apiResult);
+//    }
+
+
     @GetMapping("/stores/{id}")
-    public ResponseEntity<?> find(@PathVariable(value = "id", required = true) Long id) {
+    public ResponseEntity<?> findById(@PathVariable(value = "id", required = true) Long id) {
         StoreResponse.FindByIdStoreDTO responseDTO = storeService.findById(id);
         ApiUtils.ApiSuccess<?> apiResult = ApiUtils.success(responseDTO);
         return ResponseEntity.ok(apiResult);
