6b6765b10ee86d233cc799119266e55789d935b0
jinc20 <wh5971@pusan.ac.kr>
2023-10-15 10:31:46 +0900

    feat: Review 조회 DTO 수정 - FindByReviewIdDTO, FIndAllByStoreIdDTO
    ReviewResponse.FindByReviewIdDTO의 필드를 일부 수정하고 내부에 ReviewerDTO를 구현하며 reviewer 필드를 추가했습니다.
    FindAllByStoreIdDTO의 필드를 일부 수정했습니다.

--- matgpt/src/main/java/com/ktc/matgpt/feature_review/review/dto/ReviewResponse.java
+++ matgpt/src/main/java/com/ktc/matgpt/feature_review/review/dto/ReviewResponse.java
@@ -15,34 +15,56 @@ public class ReviewResponse {
     @Getter
     @ToString
     public static class FindByReviewIdDTO {
+        private Long storeId;
         private Long reviewId;
-        private List<FindByReviewIdDTO.ImageDTO> reviewImages;
-        private String content;
-        private int rating;
+        private ReviewerDTO reviewer;
+        private int averageCostPerPerson;
         private int peopleCount;
+        private String createdAt;
+        private double rating;
+        private int recommendCount;
+        private String content;
+        private List<FindByReviewIdDTO.ImageDTO> reviewImages;
         private int totalPrice;     // 범위로 받게 된다면 enum PriceRange 타입 이용
-        private int costPerPerson;
         private boolean isUpdated = false;
-        private String relativeTime;
-        public FindByReviewIdDTO(Review review, String relativeTime, List<ImageDTO> reviewImages) {
+
+        public FindByReviewIdDTO(Review review, ReviewerDTO reviewer, List<ImageDTO> reviewImages, String relativeTime) {
+            this.storeId = review.getStore().getId();
             this.reviewId = review.getId();
+            this.reviewer = reviewer;
             this.reviewImages = reviewImages;
             this.content = review.getContent();
             this.rating = review.getRating();
+            this.recommendCount = review.getRecommendCount();
             this.peopleCount = review.getPeopleCount();
             this.totalPrice = review.getTotalPrice();
-            this.costPerPerson = review.getCostPerPerson();
-            this.relativeTime = relativeTime;
+            this.averageCostPerPerson = review.getCostPerPerson();
+            this.createdAt = relativeTime;
             if (review.getCreatedAt() != review.getUpdatedAt()) this.isUpdated = true;
         }
 
         @Getter
         @ToString
+        public static class ReviewerDTO {
+            private String profileImage;
+            private String userName;
+            private String email;
+
+            @Builder
+            public ReviewerDTO(String profileImage, String userName, String email) {
+                this.profileImage = profileImage;
+                this.userName = userName;
+                this.email = email;
+            }
+        }
+
+        @Getter
+        @ToString
         public static class ImageDTO {
-            private String imageUrl;
+            private String image;
             private List<ImageDTO.TagDTO> tags;
             public ImageDTO(Image image, List<Tag> tags) {
-                this.imageUrl = image.getUrl();
+                this.image = image.getUrl();
                 this.tags = tags.stream()
                         .map(t -> new TagDTO(t))
                         .collect(Collectors.toList());
@@ -71,15 +93,19 @@ public class ReviewResponse {
     @ToString
     public static class FindAllByStoreIdDTO {
         private Long reviewId;
-        private List<String> imageUrls;
+        private Long userId;
+        private double rating;
         private String content;
-        private int rating;
         private LocalDateTime createdAt;
+
+        private List<String> imageUrls;
         private boolean isUpdated = false;
         private String relativeTime;
 
+
         public FindAllByStoreIdDTO(Review review, String relativeTime, List<String> imageUrls) {
             this.reviewId = review.getId();
+            this.userId = review.getUserId();
             this.imageUrls = imageUrls;
             this.content = review.getContent();
             this.rating = review.getRating();
