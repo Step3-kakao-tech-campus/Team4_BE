595b2e047782a5b67d3244ffc1f009b4896503d4
pigglehyun <sarah0661@pusan.ac.kr>
2023-10-19 01:35:17 +0900

    feat: 좋아요 누르기, 좋아요 목록 보기 controller 작성

---
+++ matgpt/src/main/java/com/ktc/matgpt/heart/storeHeart/HeartRestController.java
@@ -0,0 +1,46 @@
+package com.ktc.matgpt.heart.storeHeart;
+
+
+import com.ktc.matgpt.security.UserPrincipal;
+import com.ktc.matgpt.user.entity.User;
+import com.ktc.matgpt.utils.ApiUtils;
+import jakarta.validation.Valid;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("")
+public class HeartRestController {
+
+    private final HeartService heartService;
+
+    //
+    @GetMapping("/stores/like")
+    public ResponseEntity<?> findAllStores(@AuthenticationPrincipal UserPrincipal userPrincipal){
+        HeartResponseDTO.FindAllLikeStoresDTO heartResponseDTO = heartService.findStoresByUser(userPrincipal.getEmail());
+        ApiUtils.ApiSuccess<?> apiResult = ApiUtils.success(heartResponseDTO);
+        return ResponseEntity.ok(apiResult);
+
+    }
+
+    //특정 store에 좋아요 누르기
+    @PostMapping("/stores/{storeId}/like")
+    public ResponseEntity<?> addHeart(@PathVariable(value = "storeId", required = true) Long storeId , @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        heartService.addHeartToStore(storeId, userPrincipal.getEmail());
+        ApiUtils.ApiSuccess<?> apiResult = ApiUtils.success("success");
+        return ResponseEntity.ok(apiResult);
+    }
+
+
+    //특정 store에 좋아요 취소하기
+//    @DeleteMapping("/stores/{storeId}/like")
+//    public ResponseEntity<?> deleteHeart(@PathVariable(value = "storeId", required = true) Long storeId , @AuthenticationPrincipal UserPrincipal userPrincipal){
+//
+//    }
+}
