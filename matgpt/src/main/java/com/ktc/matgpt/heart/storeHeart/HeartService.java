a8974b939664640df8a145364e8a829885beef16
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-10-23 16:43:54 +0900

    Refactor: Replace direct repository calls with service methods in HeartService

--- matgpt/src/main/java/com/ktc/matgpt/heart/storeHeart/HeartService.java
+++ matgpt/src/main/java/com/ktc/matgpt/heart/storeHeart/HeartService.java
@@ -1,13 +1,12 @@
 package com.ktc.matgpt.heart.storeHeart;
 
 
-import com.ktc.matgpt.security.UserPrincipal;
 import com.ktc.matgpt.store.Store;
-import com.ktc.matgpt.store.StoreJPARepository;
+import com.ktc.matgpt.store.StoreService;
 import com.ktc.matgpt.user.entity.User;
-import com.ktc.matgpt.user.repository.UserRepository;
+import com.ktc.matgpt.user.service.UserService;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,42 +16,50 @@ import java.util.List;
 @RequiredArgsConstructor
 public class HeartService {
 
-    private final StoreJPARepository storeJPARepository;
-    private final UserRepository userRepository;
+    private final StoreService storeService;
+    private final UserService userService;
     private final HeartJPARepository heartJPARepository;
 
+    private final EntityManager entityManager;
 
     @Transactional
     public void addHeartToStore(Long storeId, String email){
 
-        User user = userRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("해당 회원을 찾을 수 없습니다."));
-        Store store = storeJPARepository.findById(storeId).orElseThrow(() -> new IllegalArgumentException("해당 가게를 찾을 수 없습니다."));
+        User userRef = userService.getReferenceByEmail(email);
+        Store storeRef = storeService.getReferenceById(storeId);
 
-        //이미 좋아요를 눌렀을 경우
-        if (heartJPARepository.findStoresByUserId(user.getId()).contains(store)){
+        if (isHeartAlreadyExists(userRef.getId(), storeId)) {
             throw new IllegalArgumentException("이미 하트가 눌러진 상태입니다.");
         }
 
         //없을 경우에 repo에 저장
-        Heart heart = Heart.builder().user(user).store(store).build();
+        Heart heart = Heart.builder().user(userRef).store(storeRef).build();
+
         heartJPARepository.save(heart);
     }
 
     @Transactional
-    public void deleteHeartToStore(Long storeId, String email){
-        User user = userRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("해당 회원을 찾을 수 없습니다."));
-        Store store = storeJPARepository.findById(storeId).orElseThrow(() -> new IllegalArgumentException("해당 가게를 찾을 수 없습니다."));
+    public void deleteHeartToStore(Long storeId, String email) {
 
-        //좋아요가 없을 경우 error
-        Heart heart = heartJPARepository.findHeartByUserId(user.getId()).orElseThrow(()-> new IllegalArgumentException("좋아요가 없습니다."));
+        User user = userService.findByEmail(email);
+        Store store = storeService.findById(storeId);
+
+        if (!isHeartAlreadyExists(user.getId(), storeId)) {
+            throw new IllegalArgumentException("좋아요가 없습니다.");
+        }
+
         //좋아요가 있을 경우 하트 삭제
-        heartJPARepository.delete(heart);
+        heartJPARepository.deleteByUserAndStore(user, store);
     }
 
-
-    public HeartResponseDTO.FindAllLikeStoresDTO findStoresByUser(String email) {
-        User user= userRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("해당 회원을 찾을 수 없습니다."));
+    public HeartResponseDTO.FindAllLikeStoresDTO findStoresByUserEmail(String email) {
+        User user = userService.findByEmail(email);
         List<Store> stores=  heartJPARepository.findStoresByUserId(user.getId()).stream().toList();
         return new HeartResponseDTO.FindAllLikeStoresDTO(user,stores);
     }
+
+    private boolean isHeartAlreadyExists(Long userId, Long storeId) {
+        return heartJPARepository.existsByUserIdAndStoreId(userId, storeId);
+    }
+
 }
