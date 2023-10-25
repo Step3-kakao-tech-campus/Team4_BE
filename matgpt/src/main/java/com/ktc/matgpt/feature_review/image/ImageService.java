99c497d1591c9fceae5c9b7d979c4868be3666fd
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-10-26 00:02:01 +0900

    feature: Add Image static factory method, setReview, and no-args constructor

--- matgpt/src/main/java/com/ktc/matgpt/feature_review/image/ImageService.java
+++ matgpt/src/main/java/com/ktc/matgpt/feature_review/image/ImageService.java
@@ -1,6 +1,5 @@
 package com.ktc.matgpt.feature_review.image;
 
-import com.ktc.matgpt.feature_review.review.dto.ReviewRequest;
 import com.ktc.matgpt.feature_review.review.entity.Review;
 import com.ktc.matgpt.feature_review.s3.S3Service;
 import com.ktc.matgpt.feature_review.tag.TagService;
@@ -19,28 +18,34 @@ public class ImageService {
     private final TagService tagService;
     private final S3Service s3Service;
 
-    public Image save(ReviewRequest.CreateDTO.ImageDTO imageDTO, Review review/*, String imageUrl*/) {
-        Image image = Image.builder()
-                .review(review)
-//                .url(imageUrl)
-                .url(imageDTO.getImageUrl())
-                .build();
+    public Image saveImageForReview(Review review, String imageUrl) {
+        Image image = Image.create(review, imageUrl);
         imageJPARepository.save(image);
-
         return image;
     }
 
     @Transactional
-    public void deleteAll(Long reviewId) {
+    public void deleteImagesByReviewId(Long reviewId) {
         List<Image> images = imageJPARepository.findAllByReviewId(reviewId);
         if (images.isEmpty()) {
             log.info("review-" + reviewId + ": 해당 리뷰에 삭제할 이미지가 없습니다.");
             return;
         }
+        deleteImagesAndAssociatedTags(images);
+    }
 
+    private void deleteImagesAndAssociatedTags(List<Image> images) {
         for (Image image : images) {
-            tagService.deleteAll(image.getId());
+            tagService.deleteTagsByImageId(image.getId());
             imageJPARepository.delete(image);
         }
+    }
+
+    public List<Image> getImagesByReviewId(Long reviewId){
+        return imageJPARepository.findAllByReviewId(reviewId);
+    }
+
+    public List<String> getImageUrlsByReviewId(Long reviewId){
+        return imageJPARepository.findAllImagesByReviewId(reviewId);
     }
 }
