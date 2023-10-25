78a3ec944bc7d5d1f3e9292b5d9986bfd84f0e8f
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-10-25 21:42:50 +0900

    feature: JPA repository methods to fetch foods by store and recent top 10 foods

--- matgpt/src/main/java/com/ktc/matgpt/feature_review/food/FoodJPARepository.java
+++ matgpt/src/main/java/com/ktc/matgpt/feature_review/food/FoodJPARepository.java
@@ -2,8 +2,13 @@ package com.ktc.matgpt.feature_review.food;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FoodJPARepository extends JpaRepository<Food, Long> {
     Optional<Food> findByFoodName(String foodName);
+
+    List<Food> findByStoreId(Long storeId);
+
+    List<Food> findTop10ByStoreIdOrderByCreatedAtDesc(Long storeId);
 }
