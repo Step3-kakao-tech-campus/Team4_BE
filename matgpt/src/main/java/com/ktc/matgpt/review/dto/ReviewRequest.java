2e13aba80150a1909fce189774cc4e957eeac83c
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-10-28 13:39:43 +0900

    feature: Refactor ReviewRequest DTOs for Simplified and Complete Review Creation
    - Introduced `SimpleCreateDTO` for initial review creation, streamlined with essential fields.
    - Added nested `ImageDTO` within `SimpleCreateDTO` to handle image files.
    - Implemented `CreateCompleteDTO` for storing reviews after image upload completion.
    - Included validation annotations for `peopleCount` and `totalPrice` in `CreateCompleteDTO`.
    - `CreateCompleteDTO` now contains `ImageDTO` with `imageUrl` and a list of `TagDTO`.
    - Added `TagDTO` nested class within `CreateCompleteDTO.ImageDTO` for tag details associated with an image.

--- matgpt/src/main/java/com/ktc/matgpt/review/dto/ReviewRequest.java
+++ matgpt/src/main/java/com/ktc/matgpt/review/dto/ReviewRequest.java
@@ -8,6 +8,72 @@ import java.util.List;
 
 public class ReviewRequest {
 
+    // 초기 리뷰 생성을 위한 간소화된 DTO
+    @Getter
+    @ToString
+    public class SimpleCreateDTO {
+        private List<SimpleCreateDTO.ImageDTO> reviewImages;
+        private String content;
+        private double rating;
+        private int peopleCount;
+        private int totalPrice;
+
+        public SimpleCreateDTO(List<SimpleCreateDTO.ImageDTO> reviewImages, String content,
+                         double rating, int peopleCount, int totalPrice) {
+            this.reviewImages = reviewImages;
+            this.content = content;
+            this.rating = rating;
+            this.peopleCount = peopleCount;
+            this.totalPrice = totalPrice;
+        }
+
+        @Getter
+        @ToString
+        @NoArgsConstructor(force = true)
+        public static class ImageDTO {
+            private MultipartFile image;
+        }
+    }
+
+    //이미지 업로드 후 리뷰 저장을 위한 DTO
+    @Getter
+    @ToString
+    public static class CreateCompleteDTO {
+        private List<ImageDTO> reviewImages;
+        private String content;
+        private double rating;
+        @Min(1)
+        private int peopleCount;
+        @Min(0)
+        private int totalPrice;     //TODO: enum PriceRange 타입 변환 메서드 만들어야할 것
+
+        public CreateCompleteDTO(List<ImageDTO> reviewImages, String content,
+                                 double rating, int peopleCount, int totalPrice) {
+            this.reviewImages = reviewImages;
+            this.content = content;
+            this.rating = rating;
+            this.peopleCount = peopleCount;
+            this.totalPrice = totalPrice;
+        }
+
+        @Getter
+        @ToString
+        @NoArgsConstructor(force = true)
+        public static class ImageDTO {
+            private String imageUrl;
+            private List<TagDTO> tags;
+
+            @Getter
+            @ToString
+            public static class TagDTO {
+                private String name;
+                private int locationX;
+                private int locationY;
+                private double rating;
+            }
+        }
+    }
+
     @Getter
     @ToString
     public static class CreateDTO {
