2174d8c273e80cafaef0e7e707d147075ba47d8c
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-10-28 13:36:52 +0900

    refactor: Enhance Error Handling with Custom Messages
    - Added specific error messages for various exceptional scenarios in the application.
    - Included `API_UNKNOWN_FINISH_REASON` for unexplained failures in ChatGPT API responses.
    - Added `STORE_NOT_FOUND` and `REVIEW_NOT_FOUND` for handling cases where requested stores or reviews are not found.
    - Implemented `REVIEW_PROCESS_ERROR` to address errors occurring during the review completion process.
    - Associated relevant HTTP status codes with each error type for clearer client-side interpretation.

--- matgpt/src/main/java/com/ktc/matgpt/exception/ErrorCode.java
+++ matgpt/src/main/java/com/ktc/matgpt/exception/ErrorCode.java
@@ -12,7 +12,11 @@ public enum ErrorCode {
 
     API_CONTENT_NOT_FOUND(BAD_REQUEST, 400, "[ChatGPT API] 응답을 생성하는데 실패했습니다."),
     API_REQUEST_TIMEOUT(REQUEST_TIMEOUT, 408, "[ChatGPT API] API 호출 시간이 초과됐습니다."),
-    API_UNKNOWN_FINISH_REASON(INTERNAL_SERVER_ERROR, 500, "[ChatGPT API] 알 수 없는 이유로 응답을 불러올 수 없습니다.");
+    API_UNKNOWN_FINISH_REASON(INTERNAL_SERVER_ERROR, 500, "[ChatGPT API] 알 수 없는 이유로 응답을 불러올 수 없습니다."),
+    STORE_NOT_FOUND(NOT_FOUND, 404, "[MatGPT] 요청한 상점을 찾을 수 없습니다."),
+    REVIEW_NOT_FOUND(NOT_FOUND, 404, "[MatGPT] 요청한 리뷰를 찾을 수 없습니다."),
+    REVIEW_PROCESS_ERROR(BAD_REQUEST, 400, "리뷰 완료 중 오류가 발생했습니다.");
+
 
     private final HttpStatus httpStatus;
     private final int code;
