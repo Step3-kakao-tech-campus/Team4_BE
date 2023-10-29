d5c7fa055a5c69795236bdf7c9ea68a8865ee5e4
pigglehyun <sarah0661@pusan.ac.kr>
2023-10-29 19:00:25 +0900

    feat: MarkerStores를 위한 JPARepository

--- matgpt/src/main/java/com/ktc/matgpt/store/StoreJPARepository.java
+++ matgpt/src/main/java/com/ktc/matgpt/store/StoreJPARepository.java
@@ -13,8 +13,12 @@ public interface StoreJPARepository extends JpaRepository<Store,Long> {
 
     Optional<Store> findById(Long id);
 
+    //거리안의 음식점 marker 가져오기
+    @Query(value ="SELECT * FROM store_tb WHERE latitude < :maxLat AND latitude > :minLat AND longitude < :maxLon AND longitude > :minLon  ", nativeQuery = true)
+    List<Store> findMarkers(@Param("maxLat")double maxLat, @Param("maxLon")double maxLon, @Param("minLat")double minLat, @Param("minLon")double minLon);
+
     //사용자의 위치와 음식점 거리가 가까운 순으로 음식점 불러오기
-    @Query(value ="SELECT *,ST_Distance_Sphere(POINT(:latitude, :longitude),POINT(latitude, longitude)) AS distance FROM store_tb ", nativeQuery = true)
+    @Query(value ="SELECT *,ST_Distance_Sphere(POINT(:longitude, :latitude),POINT(longitude, latitude)) AS distance FROM store_tb ", nativeQuery = true)
     List<Store> findNearestStoresWithDistance(double latitude, double longitude);
 
 
