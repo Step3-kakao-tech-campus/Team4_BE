a15bcf84102e41fa4a7e00ab69d31089344662cd
Kong Heechan <sk980919@naver.com>
2023-10-31 23:25:44 +0900

    Refactor: StoreService의 UserRepository 접근 분리

--- matgpt/src/main/java/com/ktc/matgpt/user/repository/UserRepository.java
+++ matgpt/src/main/java/com/ktc/matgpt/user/repository/UserRepository.java
@@ -1,15 +1,19 @@
 package com.ktc.matgpt.user.repository;
 
 
+import com.ktc.matgpt.user.entity.AgeGroup;
+import com.ktc.matgpt.user.entity.Gender;
 import com.ktc.matgpt.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    List<User> findAllByAgeGroupAndGender(AgeGroup ageGroup, Gender gender);
     Optional<User> findByEmail(String email);
     Boolean existsByEmail(String email);
 
--- matgpt/src/main/java/com/ktc/matgpt/user/service/UserService.java
+++ matgpt/src/main/java/com/ktc/matgpt/user/service/UserService.java
@@ -1,11 +1,14 @@
 package com.ktc.matgpt.user.service;
 
+import com.ktc.matgpt.user.entity.AgeGroup;
+import com.ktc.matgpt.user.entity.Gender;
 import com.ktc.matgpt.user.entity.User;
 import com.ktc.matgpt.user.repository.UserRepository;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @RequiredArgsConstructor
@@ -28,6 +31,10 @@ public class UserService {
     public User getReferenceByEmail(String email) {
         // 실제 엔터티를 로드하지 않고, 프록시 객체를 반환
         return entityManager.getReference(User.class, findByEmail(email).getId());
+    }
+
+    public List<User> findByAgeGroupAndGender(AgeGroup ageGroup, Gender gender) {
+        return userRepository.findAllByAgeGroupAndGender(ageGroup, gender);
     }
 
 }
