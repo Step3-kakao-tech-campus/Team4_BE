11f9bbb7e6c92cd409e6b4644fda1333ba14dc02
Kong Heechan <sk980919@naver.com>
2023-10-31 23:28:50 +0900

    Feat: 커서기반 페이지네이션 동작 수정

--- matgpt/src/main/java/com/ktc/matgpt/store/StoreJPARepository.java
+++ matgpt/src/main/java/com/ktc/matgpt/store/StoreJPARepository.java
@@ -1,49 +1,78 @@
 package com.ktc.matgpt.store;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface StoreJPARepository extends JpaRepository<Store,Long> {
 
-    Optional<Store> findById(Long id);
+    @Query(value = "SELECT s FROM Store s JOIN FETCH s.subCategory sc JOIN FETCH sc.category c WHERE s.id = :id")
+    Optional<Store> findById(@Param("id")Long id);
 
     //거리안의 음식점 marker 가져오기
-    @Query(value ="SELECT * FROM store_tb WHERE latitude < :maxLat AND latitude > :minLat AND longitude < :maxLon AND longitude > :minLon  ", nativeQuery = true)
+    @Query(value ="SELECT * FROM store_tb WHERE latitude < :maxLat AND latitude > :minLat AND longitude < :maxLon AND longitude > :minLon", nativeQuery = true)
     List<Store> findMarkers(@Param("maxLat")double maxLat, @Param("maxLon")double maxLon, @Param("minLat")double minLat, @Param("minLon")double minLon);
 
+    // mysql에서는 st_distance_sphere 함수를 사용가능하지만, 배포환경인 mariadb와 h2에서는 사용 불가하므로, 공식을 통해 거리 가져오기
     //사용자의 위치와 음식점 거리가 가까운 순으로 음식점 불러오기
-    @Query(value ="SELECT *,ST_Distance_Sphere(POINT(:longitude, :latitude),POINT(longitude, latitude)) AS distance FROM store_tb ", nativeQuery = true)
-    List<Store> findNearestStoresWithDistance(double latitude, double longitude);
+    @Query(value = "WITH DistanceCTE AS (" +
+            "SELECT s.*, " +
+            "6371 * ACOS(" +
+            "COS(RADIANS(:latitude)) * COS(RADIANS(latitude)) * " +
+            "COS(RADIANS(longitude) - RADIANS(:longitude)) + " +
+            "SIN(RADIANS(:latitude)) * SIN(RADIANS(latitude))" +
+            ") AS distance FROM store_tb s)" +
+            "SELECT * FROM DistanceCTE " +
+            "WHERE (distance > :cursor OR (distance = :cursor AND id < :lastid)) " +
+            "ORDER BY distance ASC, id DESC",
+            nativeQuery = true)
+    List<Store> findNearestStoresWithDistance(@Param("latitude")double latitude, @Param("longitude")double longitude, @Param("cursor")Double cursor, @Param("lastid")Long lastId, Pageable pageable);
 
+    //사용자의 위치와 음식점 거리가 가까운 순으로 음식점 불러오기
+    @Query(value = "WITH DistanceCTE AS (" +
+            "SELECT s.*, " +
+            "6371 * ACOS(" +
+            "COS(RADIANS(:latitude)) * COS(RADIANS(latitude)) * " +
+            "COS(RADIANS(longitude) - RADIANS(:longitude)) + " +
+            "SIN(RADIANS(:latitude)) * SIN(RADIANS(latitude))" +
+            ") AS distance FROM store_tb s)" +
+            "SELECT * FROM DistanceCTE " +
+            "ORDER BY distance ASC, id DESC",
+            nativeQuery = true)
+    List<Store> findNearestStoresWithDistance(@Param("latitude")double latitude, @Param("longitude")double longitude, Pageable pageable);
 
     //별점 높은 순으로 가게 불러오기
-    @Query("SELECT s FROM Store s WHERE s.name LIKE %:search% ORDER BY s.ratingAvg DESC , s.id")
-    List<Store> findAllByStar(@Param("search")String search,Pageable page);
+    @Query("SELECT s FROM Store s WHERE s.name LIKE %:search% ORDER BY s.ratingAvg DESC")
+    List<Store> findAllByStar(@Param("search")String search, Pageable pageable);
 
     //별점 높은 순으로 가게 불러오기 with cursor id
-    @Query("SELECT s FROM Store s WHERE s.id < :id AND s.name LIKE %:search% ORDER by s.ratingAvg DESC, s.id ")
-    List<Store> findAllByStarLessThanIdDesc(@Param("search")String search,@Param("id")Long id, Pageable page);
-
+    @Query("SELECT s FROM Store s " +
+            "WHERE s.name LIKE %:search% AND (s.ratingAvg < :cursor OR (s.ratingAvg = :cursor AND s.id < :lastid))" +
+            "ORDER by s.ratingAvg DESC, s.id DESC")
+    List<Store> findAllByStarLessThanIdDesc(@Param("search")String search, @Param("cursor")Long cursor, @Param("lastid")Long lastId, Pageable pageable);
 
     //id 내림차순으로 가게 불러오기
-    @Query("SELECT s FROM Store s WHERE s.name LIKE %:search% ORDER by s.id DESC ")
-    List<Store> findAllById(@Param("search")String search,Pageable page);
+    @Query("SELECT s FROM Store s WHERE s.name LIKE %:search% ORDER by s.id DESC")
+    List<Store> findAllById(@Param("search")String search, Pageable pageable);
 
     //id 내림차순으로 가게 불러오기 with cursor id
-    @Query("SELECT s FROM Store s WHERE s.id < :id AND s.name LIKE %:search% ORDER by  s.id  ")
-    List<Store> findByIdLessThanOrderByIdDesc(@Param("search")String search,Long id, Pageable page);
+    @Query("SELECT s FROM Store s WHERE s.id < :cursor AND s.name LIKE %:search% ORDER by s.id DESC")
+    List<Store> findByIdLessThanOrderByIdDesc(@Param("search")String search, @Param("cursor")Long cursor, Pageable pageable);
 
     //리뷰 많은 순으로 가게 불러오기
-    @Query("SELECT s FROM Store s WHERE s.name LIKE %:search% ORDER BY s.numsOfReview DESC, s.id ")
-    List<Store> findAllByReviews(@Param("search")String search,  Pageable page);
+    @Query("SELECT s FROM Store s WHERE s.name LIKE %:search% ORDER BY s.numsOfReview DESC, s.id")
+    List<Store> findAllByReviews(@Param("search")String search, Pageable pageable);
 
     //리뷰 많은 순으로 가게 불러오기 with cursor id
-    @Query("SELECT s FROM Store s WHERE s.id < :id AND s.name LIKE %:search% ORDER by s.numsOfReview DESC, s.id  ")
-    List<Store> findAllByReviewsLessThanIdDesc(@Param("search")String search,@Param("id")Long id, Pageable page);
+    @Query("SELECT s " +
+            "FROM Store s " +
+            "WHERE s.name LIKE %:search% AND (s.numsOfReview < :cursor OR (s.numsOfReview = :cursor AND s.id < :lastId))" +
+            "ORDER BY s.numsOfReview DESC, s.id DESC")
+    List<Store> findAllByReviewsLessThanIdDesc(@Param("search")String search, @Param("cursor")int cursor, @Param("lastId")Long lastId, Pageable pageable);
 }
