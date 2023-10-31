782ee2a0a2e60bb3dd19a21105fa13f8ba915d65
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-11-01 02:49:55 +0900

    feature: Enhanced Exception Messages
    - Added REVIEW_PROCESS_ERROR for handling review registration errors.
    - Introduced S3_FILE_VALIDATION_ERROR for S3 file validation issues.
    - Added INVALID_TOKEN_EXCEPTION and OAUTH2_PROCESSING_EXCEPTION for auth-related errors.

--- matgpt/src/main/java/com/ktc/matgpt/exception/ErrorCode.java
+++ matgpt/src/main/java/com/ktc/matgpt/exception/ErrorCode.java
@@ -15,8 +15,12 @@ public enum ErrorCode {
     API_UNKNOWN_FINISH_REASON(INTERNAL_SERVER_ERROR, 500, "[ChatGPT API] 알 수 없는 이유로 응답을 불러올 수 없습니다."),
     STORE_NOT_FOUND(NOT_FOUND, 404, "[MatGPT] 요청한 상점을 찾을 수 없습니다."),
     REVIEW_NOT_FOUND(NOT_FOUND, 404, "[MatGPT] 요청한 리뷰를 찾을 수 없습니다."),
-    REVIEW_PROCESS_ERROR(BAD_REQUEST, 400, "리뷰 완료 중 오류가 발생했습니다.");
+    REVIEW_PROCESS_ERROR(BAD_REQUEST, 400, "[MatGPT] 리뷰 등록 중 오류가 발생했습니다."),
+    S3_FILE_VALIDATION_ERROR(BAD_REQUEST, 400, "[MatGPT] S3파일 검증 중에 오류가 발생했습니다."),
 
+    //Auth
+    INVALID_TOKEN_EXCEPTION(INTERNAL_SERVER_ERROR, 500, "[MatGPT] 유효하지 않은 토큰입니다."),
+    OAUTH2_PROCESSING_EXCEPTION(UNAUTHORIZED, 401, "[MatGPT] OAuth2 인증 처리 중 오류가 발생했습니다.");
 
     private final HttpStatus httpStatus;
     private final int code;
