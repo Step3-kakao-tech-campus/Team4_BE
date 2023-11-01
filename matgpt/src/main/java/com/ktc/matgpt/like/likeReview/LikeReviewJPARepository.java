d13d3f938bff1564b2a7316aa8da8df03fd77269
jinc20 <wh5971@pusan.ac.kr>
2023-11-01 13:41:37 +0900

    feat: LikeReview 레포지토리 메서드 변경 및 추가

--- matgpt/src/main/java/com/ktc/matgpt/like/likeReview/LikeReviewJPARepository.java
+++ matgpt/src/main/java/com/ktc/matgpt/like/likeReview/LikeReviewJPARepository.java
@@ -3,16 +3,19 @@ package com.ktc.matgpt.like.likeReview;
 import com.ktc.matgpt.review.entity.Review;
 import com.ktc.matgpt.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
 import java.util.List;
 
 @Repository
 public interface LikeReviewJPARepository extends JpaRepository<LikeReview, Long> {
-    List<Review> findLikedReviewsByUserId(Long id);
+    @Query("select lr from LikeReview lr join fetch lr.user join fetch lr.review where lr.user.id = :userId")
+    List<LikeReview> findAllByUserId(Long userId);
 
     void deleteByUserAndReview(User userRef, Review reviewRef);
+
+    void deleteAllByReviewId(Long reviewId);
 
     boolean existsByUserAndReview(User userRef, Review reviewRef);
 }
