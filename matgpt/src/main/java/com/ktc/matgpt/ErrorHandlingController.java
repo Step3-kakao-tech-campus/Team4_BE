31df2d5a98d1b59d2f8b01fd6d89b8c113fb03dd
heechan <sk980919@naver.com>
2023-11-02 23:47:03 +0900

    Feat: 기본 Error page와 Exception 처리 고도화

---
+++ matgpt/src/main/java/com/ktc/matgpt/ErrorHandlingController.java
@@ -0,0 +1,45 @@
+package com.ktc.matgpt;
+
+import com.ktc.matgpt.utils.ApiUtils;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorHandlingController implements ErrorController {
+
+    @RequestMapping("/error")
+    public ResponseEntity<?> handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+            switch (statusCode) {
+                case (401) -> {
+                    ApiUtils.ApiFail apiFail = ApiUtils.fail(HttpStatus.UNAUTHORIZED.value(), "로그인이 필요한 페이지입니다.");
+                    return new ResponseEntity<>(apiFail, HttpStatus.UNAUTHORIZED);
+                }
+                case (403) -> {
+                    ApiUtils.ApiFail apiFail = ApiUtils.fail(HttpStatus.FORBIDDEN.value(), "접근할 권한이 없는 페이지입니다.");
+                    return new ResponseEntity<>(apiFail, HttpStatus.FORBIDDEN);
+                }
+                case (404) -> {
+                    ApiUtils.ApiFail apiFail = ApiUtils.fail(HttpStatus.NOT_FOUND.value(), "존재하지 않는 페이지입니다.");
+                    return new ResponseEntity<>(apiFail, HttpStatus.NOT_FOUND);
+                }
+                default -> {
+                    ApiUtils.ApiFail apiFail = ApiUtils.fail(statusCode, "알 수 없는 에러가 발생했습니다.");
+                    return new ResponseEntity<>(apiFail, HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+            }
+        }
+
+        ApiUtils.ApiFail apiFail = ApiUtils.fail(HttpStatus.INTERNAL_SERVER_ERROR.value(), "내부 서버 에러가 발생했습니다.");
+        return new ResponseEntity<>(apiFail, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
+
