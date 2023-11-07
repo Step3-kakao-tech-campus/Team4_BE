a21b99402fce1395d5fb04a5d1d6b1369cedbf88
sk980919 <sk980919@kakao.com>
2023-11-07 14:40:19 +0900

    config 수정

--- src/main/java/com/ktc/matgpt/chatgpt/config/HttpConnectionProxyConfig.java
+++ src/main/java/com/ktc/matgpt/chatgpt/config/HttpConnectionProxyConfig.java
@@ -1,5 +1,6 @@
 package com.ktc.matgpt.chatgpt.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
