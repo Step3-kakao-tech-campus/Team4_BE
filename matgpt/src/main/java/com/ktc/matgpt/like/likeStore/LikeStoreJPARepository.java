bee583205f7b40a9b5efeb8c425c72aeecd2282b
jinc20 <wh5971@pusan.ac.kr>
2023-11-08 19:49:10 +0900

    refactor: LikeStore 레포지토리 조회 쿼리문, 반환타입 수정

--- matgpt/src/main/java/com/ktc/matgpt/like/likeStore/LikeStoreJPARepository.java
+++ matgpt/src/main/java/com/ktc/matgpt/like/likeStore/LikeStoreJPARepository.java
@@ -2,6 +2,7 @@ package com.ktc.matgpt.like.likeStore;
 
 import com.ktc.matgpt.store.Store;
 import com.ktc.matgpt.user.entity.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -17,6 +18,10 @@ public interface LikeStoreJPARepository extends JpaRepository<LikeStore, Long> {
 
     @Query( "select h.store from LikeStore h where h.user.id = :userId")
     List<Store> findLikedStoresByUserId(Long userId);
+
+    @Query( "select h from LikeStore h join fetch h.user join fetch h.store " +
+            "where h.user.id = :userId and h.id < :cursorId order by h.id desc")
+    List<LikeStore> findLikedStoresByUserId(Long userId, Long cursorId, Pageable page);
     boolean existsByUserAndStore(User userRef, Store storeRef);
     void deleteByUserAndStore(User user, Store store);
 
