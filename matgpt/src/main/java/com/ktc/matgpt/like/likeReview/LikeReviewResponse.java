af9eadb0e8857a87b4760738a32e00991d5a8692
jinc20 <wh5971@pusan.ac.kr>
2023-11-10 15:14:04 +0900

    refactor: Review DTO 수정
    Review, LikeReview DTO의 클래스명과 필드를 수정, 추가했습니다.
    기존 imageUrl 리스트 필드를 대표 이미지 하나의 url로 수정했습니다.

--- matgpt/src/main/java/com/ktc/matgpt/like/likeReview/LikeReviewResponse.java
+++ matgpt/src/main/java/com/ktc/matgpt/like/likeReview/LikeReviewResponse.java
@@ -9,24 +9,28 @@ import java.time.LocalDateTime;
 public class LikeReviewResponse {
 
     @Getter @Setter
-    public static class FindLikeReviewPageDTO {
-        private Long id;
+    public static class LikeReviewDTO {
+        private Long reviewId;
         private int rating;
         private String content;
         private LocalDateTime createdAt;
         private String relativeTime;
+        private String imageUrl;
+        private Long storeId;
         private String storeImage;
         private String storeName;
         private int peopleCount;
         private String reviewerName;
         private String reviewerImage;
 
-        public FindLikeReviewPageDTO(Review review, String relativeTime) {
-            this.id = review.getId();
+        public LikeReviewDTO(Review review, String relativeTime, String imageUrl) {
+            this.reviewId = review.getId();
             this.rating = review.getRating();
             this.content = review.getContent();
             this.createdAt = review.getCreatedAt();
             this.relativeTime = relativeTime;
+            this.imageUrl = imageUrl;
+            this.storeId = review.getStore().getId();
             this.storeImage = review.getStore().getStoreImageUrl();
             this.storeName = review.getStore().getName();
             this.peopleCount = review.getPeopleCount();
